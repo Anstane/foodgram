f59a2f3f41357a313e9bcf73e5a54ae78bfa3fab
Mikhail Moskovkin <anstane228@gmail.com>
2023-06-03 15:28:08 +0300

    Updating User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
+    """Класс экземпляра пользователя."""
+
+    USER = 'user'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = (
+        (USER, 'Пользователь'),
+        (ADMIN, 'Администратор')
+    )
+
     username = models.CharField(
         'Логин',
         max_length=50,
@@ -21,6 +31,27 @@ class User(AbstractUser):
         'Фамилия',
         max_length=50
     )
+    password = models.CharField(
+        'Пароль',
+        max_length=50
+    )
+    is_subscribed = models.BooleanField(
+        'Подписка на авторов',
+        default=False
+    )
+
+    class Meta:
+        ordering = ('username')
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    @property
+    def is_user(self):
+        return self.role == self.USER
 
     def __str__(self):
         return self.username
@@ -28,18 +59,29 @@ class User(AbstractUser):
 
 class Subscribe(models.Model):
     user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='user_subscribe'
     )
-    following = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='author_subscribe'
     )
 
     class Meta:
+        verbose_name = 'subscribe'
+        verbose_name_plural = 'subscribers'
         constraints = (
             models.UniqueConstraint(
-                fields=['user', 'following'],
-                name='unique_user_following'
+                fields=['user', 'author'],
+                name='unique_user_author'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('author')),
+                name='prevent_self_follow'
             ),
         )
+    
+    def __str__(self):
+        return f'{self.user} {self.author}'
