7a4e552e70dbd695e635e717274744a70a9e9cd5
Mikhail Moskovkin <anstane228@gmail.com>
2023-06-22 11:42:07 +0300

    Fix for filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -46,10 +46,10 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(in_favorite__user == self.request.user)
+            return queryset.filter(in_favorite__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(in_shopping_cart__user == self.request.user)
+            return queryset.filter(in_shopping_cart__user=self.request.user)
         return queryset
